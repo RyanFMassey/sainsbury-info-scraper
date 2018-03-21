8b89dbe4b94a909667f0aeb6f81b113d8faa741f
Ryan <Ryan@Ryan-PC>
2018-03-21 20:11:24 +0000

    Added Controller, PageScraper and Product classes
    Product class contains variables w/ getters and setters

---
+++ BerryInfoScraper/src/main/java/models/Product.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.math.BigDecimal;
+
+public class Product {
+	String title;
+	BigDecimal unitPrice;
+	int kcalPer100g;
+	String description;
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+	
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	public int getKcalPer100g() {
+		return kcalPer100g;
+	}
+	
+	public void setKcalPer100g(int kcalPer100g) {
+		this.kcalPer100g = kcalPer100g;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
