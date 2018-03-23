36cb1f0944306775af87bc8a80c16b28fdb8f0a3
Ryan <Ryan@Ryan-PC>
2018-03-23 12:30:15 +0000

    Updated Controller to allow multiple entries before terminating.

--- BerryInfoScraper/src/main/java/controller/Controller.java
+++ BerryInfoScraper/src/main/java/controller/Controller.java
@@ -13,23 +13,29 @@ import utils.ProductToJSON;
 public class Controller {
 
 	public static void main(String args[]) {
+		Boolean exit = false;
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter a url, or enter 'd' to use the default link.");
-		String url = s.nextLine();
+		
+		do {
+			System.out.println("Enter a url, or enter 'd' to use the default link, or type exit to terminate.");
+			String url = s.nextLine();
+			
+			if (url.equals("exit")) {
+				exit = true;
+			} else  {
+				if (url.equals("d")) {
+					url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";	
+				}
+				
+				PageScraper ps = new PageScraper();
+				Document doc = ps.getHtmlPage(url);
+				List<Product> berries = ps.getAllProducts(doc);
+				ProductToJSON pdtj = new ProductToJSON();
+				Results r = new Results(berries);
+				System.out.println(pdtj.resultsToJSON(r));
+			}
+		} while (!exit);
+		
 		s.close();
-		if (url.equals("d")) {
-			url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";	
-		}
-		
-		PageScraper ps = new PageScraper();
-		Document doc = ps.getHtmlPage(url);
-		List<Product> berries = ps.getAllProducts(doc);
-		ProductToJSON pdtj = new ProductToJSON();
-		Results r = new Results(berries);
-		
-		
-		System.out.println(pdtj.resultsToJSON(r));
-	
 	}
-	
 }
